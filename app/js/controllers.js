c88ca2744e38737539eb5a9bc367074c0e49741d
Steven Inouye <steven.inouye91@gmail.com>
2014-08-19 17:23:07 -0700

    added bower

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -3,8 +3,12 @@
 /* Controllers */
 
 angular.module('myApp.controllers', [])
-  .controller('MyCtrl1', ['$scope', function($scope) {
+  .controller('MyCtrl1', ['$scope','$http', function($scope, $http) {
+    $http.get('dates/dates.json').success(function(data){
+      $scope.dates = data
+    });
 
+    $scope.orderProp = 'name'
   }])
   .controller('MyCtrl2', ['$scope', function($scope) {
 
